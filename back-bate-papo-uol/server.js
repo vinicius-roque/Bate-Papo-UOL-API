d6300ce7f65e1fa292829abd434e3452406279cc
Vinícius Roque <viniciusfr6@gmail.com>
2022-08-31 12:40:30 -0300

    docs and feat(back-bate-papo-uol): including files, installing tools and inicializing server

---
+++ back-bate-papo-uol/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send("OK");
+});
+
+server.listen(5000, () => console.log("Listening on port 5000"));
