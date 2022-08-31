effa962f7c387ce2e136c1a19d311a3721a2fb22
Vinícius Roque <viniciusfr6@gmail.com>
2022-08-31 15:37:57 -0300

    feat(server.js): implementation of some requisits of post and get

--- back-bate-papo-uol/server.js
+++ back-bate-papo-uol/server.js
@@ -5,8 +5,30 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-    res.send("OK");
+server.post('/participants', (req, res) => {
+    const { name } = req.body;
+
+    if(name === "") {
+        res.status(422);
+    }
+
+    res.send(name);
+});
+
+server.get('/participants', (req, res) => {
+    res.send();
+});
+
+server.post('/messages', (req, res) => {
+    const { to, text, type } = req.body;
+    
+    req.header(User);
+
+    if((to === "" || text === "") || (type !== "message" || type !== "private_message") || from === false) {
+        res.status(422);
+    }
+
+    res.status(201);
 });
 
 server.listen(5000, () => console.log("Listening on port 5000"));
