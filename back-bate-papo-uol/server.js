06d4800f52caf7814d977f60af8111cec99a44e6
Vinícius Roque <viniciusfr6@gmail.com>
2022-08-31 15:50:53 -0300

    feat(server.js): start of get/messages implementation

--- back-bate-papo-uol/server.js
+++ back-bate-papo-uol/server.js
@@ -16,19 +16,23 @@ server.post('/participants', (req, res) => {
 });
 
 server.get('/participants', (req, res) => {
-    res.send();
+    const { name } = req.body;
+    res.send(name);
 });
 
 server.post('/messages', (req, res) => {
     const { to, text, type } = req.body;
     
-    req.header(User);
-
     if((to === "" || text === "") || (type !== "message" || type !== "private_message") || from === false) {
         res.status(422);
     }
 
     res.status(201);
+});
+
+server.get('/messages', (req, res) => {
+    const { text } = req.body;
+    res.send(text);
 });
 
 server.listen(5000, () => console.log("Listening on port 5000"));
