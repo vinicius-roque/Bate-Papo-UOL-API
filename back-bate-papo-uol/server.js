b9fd7a5206054b018e5755b9567e29fe81928513
Vinícius Roque <viniciusfr6@gmail.com>
2022-09-01 16:27:41 -0300

    feat(server.js): instaling and using dotenv

--- back-bate-papo-uol/server.js
+++ back-bate-papo-uol/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 server.post('/participants', (req, res) => {
     const { name } = req.body;
